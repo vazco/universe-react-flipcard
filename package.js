af4db4515daf8f297d7a96c144cd675adfc32560
cristo-rabani <cristo.rabani@gmail.com>
2015-11-24 05:08:41 +0100

    fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'universe:react-flipcard',
-  version: '0.1.1',
+  version: '0.1.2',
   // Brief, one-line summary of the package.
   summary: 'React flip card component',
   // URL to the Git repository containing the source code for this package.
